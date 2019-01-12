31c9090a06194253f5c5a663c6f5457e6bc085a1
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-01-12 19:57:07 +0200

    videp fix

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -11,55 +11,24 @@ export class SliderComponent implements OnInit {
   @ViewChild('myVideo') videoplayer: any;
   public rightposition: string;
   public counter: number = 0;
-  public images: string[] = ['/assets/africa1.jpeg',
-                             '/assets/africa2.jpeg',
-                             '/assets/africa3.jpeg',
-                             '/assets/africa4.jpeg',
-                             '/assets/africa5.jpeg',
-                             '/assets/africa6.jpeg',
-                             '/assets/africa7.jpeg',
-                             '/assets/africa8.jpeg',
-                             '/assets/africa9.jpeg',
-                            ];
-
-  public images2: string[] = ['/assets/africa10.jpeg',
-                              '/assets/africa11.jpeg',
-                              '/assets/africa12.jpeg',
-                              '/assets/africa13.jpeg',
-                              '/assets/africa14.jpeg',
-                              '/assets/africa15.jpeg',
-                              '/assets/africa16.jpeg',
-                              '/assets/africa17.jpeg',
-                              '/assets/africa18.jpeg',
-                            ];
-  public imagesArray1: string[];
-  public imagesArray2: string[];                          
-
   public firstTitle: boolean;
   public secondTitle: boolean;
   public thirdTitle: boolean;
-  public firstGallery: boolean = true;
-  public secondGallery: boolean = false;
-  public thirdGallery: boolean = false;
   public firstPage: boolean;
   public secondPage: boolean;
 
   constructor() { }
 
+  ngAfterViewInit() {
+    console.log("afterinit");
+    setTimeout(() => {
+      this.videoplayer.nativeElement.play();
+    }, 1000);
+  }
+
   ngOnInit() {
     this.firstPage = true;
-    this.videoplayer.nativeElement.play();
-    
-    // setTimeout(() => {
-    //   this.firstGallery = false;
-    //   this.secondGallery = true;
-    // }, 4000);
-
-    // setTimeout(() => {
-    //   this.secondGallery = false;
-    //   this.thirdGallery = true;
-    // }, 8000);
-
+  
     setTimeout(() => {
       this.firstTitle = true;
     }, 10000);
@@ -78,17 +47,5 @@ export class SliderComponent implements OnInit {
       this.firstPage = false;
       this.secondPage = true;
     }, 20000);
-  }
-
-  /**
-   * Mix the desired array
-   * @param arrayName 
-   */
-  public shakeArray(arrayName: string):string{
-    if(arrayName == "images"){
-        return this.images[Math.floor(Math.random() * this.images.length)];
-    }else{
-      return this.images2[Math.floor(Math.random() * this.images2.length)];
-    }
   }
 }
